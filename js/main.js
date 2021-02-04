2e0325e3a843c729f02c3219b71daca3c788555d
Stas <stas001w@mail.ru>
2021-02-04 23:25:16 +0300

    Правки функций

--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@
 // console.log(getRandomNumber);
 
 // includes all numbers
-function getRandomNumber(min, max) {
+const getRandomNumber = function(min, max) {
 // Comparison of numbers
   if (min < 0 || max <= 0 || max < min) {
     return 'Неверный диапазон чисел'
   }
-  return +Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 getRandomNumber(0, 100);
 // console.log(getRandomNumber(0, 100));
 
 // function with floating point
-function getFloatRandomNumber(min, max, point) {
+const getFloatRandomNumber = function(min, max, point) {
   if (min < 0 || max <= 0 || max < min) {
     return 'Неверный диапазон чисел'
   }
