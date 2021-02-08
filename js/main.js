418fac1febe15aee07ab50556d752406263ddf39
Stas <stas001w@mail.ru>
2021-02-09 01:34:33 +0300

    Больше деталий, правильно ли пишу

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 // helper is MDN Web Docs
+
 // let getRandomNumber = Math.round(Math.random() * 4);
+
 // console.log(getRandomNumber);
 
 // includes all numbers
+
 const getRandomNumber = function(min , max) {
 // Comparison of numbers
   if (!(typeof min === 'number') || !(typeof max === 'number')) {
@@ -26,5 +29,42 @@ const getFloatRandomNumber = function(min, max, point) {
   }
   return +(Math.random() * (max - min) + min).toFixed(point);
 }
-getFloatRandomNumber(0, 100, 2);
+// getFloatRandomNumber(0, 100, 2);
 // console.log(getFloatRandomNumber(0, 100, 2));
+
+const All_OFFERS = 10;
+const author = {
+  avatar: 'img/avatars/user0' + getRandomNumber(1, 8) + '.png',
+};
+// author;
+// console.log(author);
+const OFFER = {
+  title: 'Проживание',
+  price: 6500,
+  type: 'house',
+  rooms: 2,
+  guests: 4,
+  checkin: '14:00',
+  checkout: '12:00',
+  features: [
+    'wifi',
+    'dishwasher',
+    'parking',
+    'washer',
+    'elevator',
+    'conditioner'],
+  description: 'Прекрасный дом отдыха',
+  photos: [
+    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
+};
+
+const locationOffer = function() {
+  return {
+    x: +getFloatRandomNumber(35.65, 35.7, 5),
+    y: +getFloatRandomNumber(139.7, 139.8, 5),
+  };
+};
+// console.log(locationOffer());
+
