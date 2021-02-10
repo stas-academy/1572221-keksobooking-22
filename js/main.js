5b115ebe9576ad0dc848c0006d2dbeaebc641374
Stas <stas001w@mail.ru>
2021-02-10 21:13:50 +0300

    Задание: Больше деталей исправление

--- js/main.js
+++ js/main.js
@@ -6,11 +6,8 @@ const getRandomNumber = (min , max) => {
     return 'Неверный диапазон чисел'
   }
   return Math.floor(Math.random() * (max - min + 1) + min);
-}
-getRandomNumber(0, 100);
-// console.log(getRandomNumber(0, 100));
+};
 
-// function with floating point
 const getFloatRandomNumber = (min, max, point) => {
   if (!(typeof min === 'number') || !(typeof max === 'number') || !(typeof point === 'number')) {
     return 'Введите число';
@@ -19,9 +16,7 @@ const getFloatRandomNumber = (min, max, point) => {
     return 'Неверный диапазон чисел'
   }
   return +(Math.random() * (max - min) + min).toFixed(point);
-}
-// getFloatRandomNumber(0, 100, 2);
-// console.log(getFloatRandomNumber(0, 100, 2));
+};
 
 const TITLE = [
   'Дома',
@@ -34,17 +29,20 @@ const PRICE =[
   7800,
   9900,
 ];
+
 const TYPE = [
   'palace',
   'flat',
   'house',
   'bungalow',
 ];
+
 const ROOMS = [
   1,
   2,
   3,
 ];
+
 const GUESTS =[
   1,
   2,
@@ -53,16 +51,19 @@ const GUESTS =[
   5,
   6,
 ];
+
 const CHECK_IN = [
   '12:00',
   '13:00',
   '14:00',
 ];
+
 const CHECK_OUT = [
   '12:00',
   '13:00',
   '14:00',
 ];
+
 const FEATURES =[
   'wifi',
   'dishwasher',
@@ -71,11 +72,13 @@ const FEATURES =[
   'elevator',
   'conditioner',
 ];
+
 const DESCRIPTION =[
   'Прекрасный вид на море',
   'Прекрасный вид на сад',
   'Прекрасный вид на горы',
 ];
+
 const PHOTOS =[
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
@@ -88,33 +91,30 @@ const getRandomArrayElement = (elements) => {
   return elements[getRandomNumber(0, elements.length - 1)];
 };
 
-const announcement = () => {
+const getAnnouncement = () => {
   return {
     author: {
       avatar: 'img/avatars/user0' + getRandomNumber(1, 8) + '.png',
     },
     offer: {
-      TITLE: getRandomArrayElement(TITLE),
-      ADDRESS: [location.x, location.y],
-      PRICE: getRandomArrayElement(PRICE),
-      TYPE: getRandomArrayElement(TYPE),
-      ROOMS: getRandomArrayElement(ROOMS),
-      GUESTS: getRandomArrayElement(GUESTS),
-      CHECK_IN: getRandomArrayElement(CHECK_IN),
-      CHECK_OUT: getRandomArrayElement(CHECK_OUT),
-      FEATURES: getRandomArrayElement(FEATURES),
-      DESCRIPTION: getRandomArrayElement(DESCRIPTION),
-      PHOTOS: getRandomArrayElement(PHOTOS),
+      title: getRandomArrayElement(TITLE),
+      address: [location.x, location.y],
+      price: getRandomArrayElement(PRICE),
+      type: getRandomArrayElement(TYPE),
+      rooms: getRandomArrayElement(ROOMS),
+      guests: getRandomArrayElement(GUESTS),
+      check_in: getRandomArrayElement(CHECK_IN),
+      check_out: getRandomArrayElement(CHECK_OUT),
+      features: getRandomArrayElement(FEATURES),
+      description: getRandomArrayElement(DESCRIPTION),
+      photos: getRandomArrayElement(PHOTOS),
     },
     location: {
-      x: +getFloatRandomNumber(35.65, 35.7, 5),
-      y: +getFloatRandomNumber(139.7, 139.8, 5),
+      x: getFloatRandomNumber(35.65, 35.7, 5),
+      y: getFloatRandomNumber(139.7, 139.8, 5),
     },
   };
 };
-announcement();
 
-const similarAnnouncement = new Array(OFFERS_COUNT).fill(null).map(() => announcement());
+const similarAnnouncement = new Array(OFFERS_COUNT).fill(null).map(() => getAnnouncement());
 similarAnnouncement;
-// console.log(similarAnnouncement);
-
