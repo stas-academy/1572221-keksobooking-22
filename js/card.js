0d2dec0e8fc1246c373cfbd84922a56e1ac595b2
Stas <stas001w@mail.ru>
2021-03-19 14:46:35 +0300

    Правки

--- js/card.js
+++ js/card.js
@@ -1,4 +1,4 @@
-const TYPES = {
+const typeOfHousing = {
   palace: 'Дворец',
   flat: 'Квартира',
   house: 'Дом',
@@ -13,7 +13,7 @@ const createCard = (cardAds) => {
   addElement.querySelector('.popup__title').textContent = cardAds.offer.title;
   addElement.querySelector('.popup__text--address').textContent = cardAds.offer.address;
   addElement.querySelector('.popup__text--price').textContent = cardAds.offer.price + ' р/ночь';
-  addElement.querySelector('.popup__type').textContent = TYPES[cardAds.offer.type];
+  addElement.querySelector('.popup__type').textContent = typeOfHousing[cardAds.offer.type];
   addElement.querySelector('.popup__text--capacity').textContent = cardAds.offer.rooms + '  комнаты для ' + cardAds.offer.guests + '  гостей';
   addElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + cardAds.offer.check_in + ', выезд до ' + cardAds.offer.check_out;
   addElement.querySelector('.popup__description').textContent = cardAds.offer.description;
